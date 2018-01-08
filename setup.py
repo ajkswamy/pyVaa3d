2708a8b186570c2391fac3f917ebe382a3c1bd73
Ajayrama Kumaraswamy <ajkumaraswamy@tutamail.com>
2018-01-08 13:41:24 +0100

    added setup file, manifest file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name="py_vaa3d",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    packages=find_packages(exclude=["^\.", "tests*", "*egg-info"]),
+    exclude_package_data={'': ["Readme.txt"]},
+    install_requires=["pandas>=0.20",
+                      "xlrd>=1.0",
+                      "openpyxl>=2.4.7",
+                      "pathlib2>=2.2.1",
+                      "pillow",
+                      "psutil"],
+    python_requires=">=3.6",
+    package_data={"pyVaa3d": ["bashScripts/*.sh"]}
+)
